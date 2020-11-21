b5baa415be953fcf34fefb1cc44dfa93dbea3c51
wmxl <294036748@qq.com>
2020-11-21 23:42:08 +0800

    [yiming] solution1 for problem2

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
@@ -1,13 +1,22 @@
 package com.thoughtworks.capability.gtb.demospringconfig;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class DemoSpringConfigApplicationTests {
 
+	@Autowired
+	private LevelController levelController;
+
 	@Test
 	void contextLoads() {
+		String result = levelController.getLevel();
+		String expect = "advanced";
+		assertThat(result.equals(expect));
 	}
 
 }
